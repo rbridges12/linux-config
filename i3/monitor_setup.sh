aa4fa3d9f0af127a2f30ab032d3223fa58ec1690
Riley Bridges <rlybrdgs@gmail.com>
2023-05-14 16:36:03 -0400

    updated i3 and added talon

---
+++ i3/monitor_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# xrdb -merge ~/.Xresources
+
+# black magic to get my monitor to connect
+xrandr --output DP-1 --auto
+
+# set display configuration based on which monitors are connected
+NUM_DISPLAYS=$(xrandr -q | grep " connected" | wc -l)
+echo "$NUM_DISPLAYS displays connected"
+
+# triple monitor setup
+if [[ $NUM_DISPLAYS == "3" ]]; then
+    xrandr --output DP-3 --primary --auto \
+           --output DP-1 --scale 1.2x1.2 --auto --right-of DP-3 \
+           --output eDP-1 --scale 1x1 --auto --below DP-3
+
+# single monitor setup
+elif [[ $NUM_DISPLAYS == "1" ]]; then
+    xrandr --output eDP-1 --primary --auto
+
+# double monitor setup
+  elif [[ $NUM_DISPLAYS == "2" ]]; then
+    xrandr --output eDP-1 --off \
+           --output DP-1 --primary --auto --scale 1x1
+    echo "2 monitors set"
+fi
+
+# feh --bg-fill ~/.dwm/gorskii_lilac.jpg
+# feh --bg-fill ~/.dwm/dirt_road_painting.png
+# feh --bg-fill ~/.config/i3/background.jpg
+# feh --bg-fill ~/.config/i3/dog_computer.jpg
+feh --bg-fill ~/.config/i3/night_mountain_background.jpg
+
+
+# picom --config ~/.config/picom/picom.conf
